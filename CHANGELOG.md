af504bffee0f825b1d0dddf6d538d03cf6017bc8
Diego Garcia <drgarcia1986@gmail.com>
2020-10-20 10:32:29 -0300

    Remove uWSGI Lock and using multiprocessing.Lock instead

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 Changelog
 =========
 
+[NEXT_RELASE]
+------------------
+- Remove uwsgi lock and use `multiprocessing.Lock` instead
+
 0.2.0 (2020-10-19)
 ------------------
 - Using lock on method `SharedMemoryDict.clear()`
--- shared_memory_dict/lock.py
+++ shared_memory_dict/lock.py
@@ -1,15 +1,28 @@
 import os
 from functools import wraps
 
-if os.getenv('SHARED_MEMORY_USE_UWSGI_LOCK') == '1':
-    from uwsgidecorators import lock
+if os.getenv('SHARED_MEMORY_USE_LOCK') == '1':
+    from multiprocessing import Lock
 else:
 
-    def _dummy_lock(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
+    class Lock:  # type: ignore
+        def acquire(self):
+            pass
+
+        def release(self):
+            pass
+
+
+_lock = Lock()
+
+
+def lock(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        _lock.acquire()
+        try:
             return func(*args, **kwargs)
+        finally:
+            _lock.release()
 
-        return wrapper
-
-    lock = _dummy_lock
+    return wrapper
