luizalabs/shared-memory-dict

commit d1a564d24ae474d0f1f1d5ff2e16bdf2047919a9

Create serializers for json and pickle

shared_memory_dict/serializers.py
@@ -1,0 +1,27 @@
+import json
+import pickle
+from typing import Protocol
+
+
+class SharedMemoryDictSerializer(Protocol):
+    def dumps(self, obj: dict) -> bytes:
+        ...
+
+    def loads(self, data: bytes) -> dict:
+        ...
+
+
+class JSONSerializer:
+    def dumps(self, obj: dict) -> bytes:
+        return json.dumps(obj).encode()
+
+    def loads(self, data: bytes) -> dict:
+        return json.loads(data)
+
+
+class PickleSerializer:
+    def dumps(self, obj: dict) -> bytes:
+        return pickle.dumps(obj, pickle.HIGHEST_PROTOCOL)
+
+    def loads(self, data: bytes) -> dict:
+        return pickle.loads(data)

tests/test_serializers.py
@@ -1,0 +1,54 @@
+import pytest
+
+from shared_memory_dict.serializers import JSONSerializer, PickleSerializer
+
+
+class TestPickleSerializer:
+    @pytest.fixture
+    def pickle_serializer(self):
+        return PickleSerializer()
+
+    @pytest.fixture
+    def bytes_content(self):
+        return (
+            b'\x80\x05\x95\x12\x00\x00\x00\x00\x00\x00\x00}\x94\x8c\x03key'
+            b'\x94\x8c\x05value\x94s.'
+        )
+
+    @pytest.fixture
+    def dict_content(self):
+        return {"key": "value"}
+
+    def test_loads_should_transform_bytes_into_dict(
+        self, pickle_serializer, bytes_content, dict_content
+    ):
+        assert pickle_serializer.loads(bytes_content) == dict_content
+
+    def test_dumps_should_transform_dict_into_bytes(
+        self, pickle_serializer, bytes_content, dict_content
+    ):
+        assert pickle_serializer.dumps(dict_content) == bytes_content
+
+
+class TestJSONSerializer:
+    @pytest.fixture
+    def json_serializer(self):
+        return JSONSerializer()
+
+    @pytest.fixture
+    def bytes_content(self):
+        return b'{"key": "value"}'
+
+    @pytest.fixture
+    def dict_content(self):
+        return {"key": "value"}
+
+    def test_loads_should_transform_bytes_into_dict(
+        self, json_serializer, bytes_content, dict_content
+    ):
+        assert json_serializer.loads(bytes_content) == dict_content
+
+    def test_dumps_should_transform_dict_into_bytes(
+        self, json_serializer, bytes_content, dict_content
+    ):
+        assert json_serializer.dumps(dict_content) == bytes_content

commit ee0479a0f3dd827b25c26fad94971b2545f6436e

fix[JSONSerializer]: fix unserializing of JSON data
1. The json module does not support memoryview() objects to load().

>>> import shared_memory_dict
>>> s = shared_memory_dict.SharedMemoryDict('test', 1000, serializer=shared_memory_dict.serializers.JSONSerializer())
>>> s
Fail to load data: TypeError('the JSON object must be str, bytes or bytearray, not memoryview')
{}
>>> s['foo']  = 1
Fail to load data: TypeError('the JSON object must be str, bytes or bytearray, not memoryview')
>>> s
Fail to load data: TypeError('the JSON object must be str, bytes or bytearray, not memoryview')
{}

2. The memoryview() data initially contains only null bytes (with the
size of the data)

3. If the size decreases, the memory is not overwritten with null bytes.
Therefore invalid JSON data is part of the memory.
Always append a null byte to the dumped data and read only until a null
byte when loading data.

Fixes: d1a564d24ae474d0f1f1d5ff2e16bdf2047919a9

shared_memory_dict/dict.py
@@ -157,7 +157,10 @@ class SharedMemoryDict:
         try:
             return SharedMemory(name=name)
         except FileNotFoundError:
-            return SharedMemory(name=name, create=True, size=size)
+            shm = SharedMemory(name=name, create=True, size=size)
+            data = self._serializer.dumps({})
+            shm.buf[: len(data)] = data
+            return shm
 
     def _save_memory(self, db: Dict[str, Any]) -> None:
         data = self._serializer.dumps(db)
@@ -168,7 +171,7 @@ class SharedMemoryDict:
 
     def _read_memory(self) -> Dict[str, Any]:
         try:
-            return self._serializer.loads(self._memory_block.buf)
+            return self._serializer.loads(self._memory_block.buf.tobytes())
         except Exception as exc:
             logger.warning(f"Fail to load data: {exc!r}")
             return {}

shared_memory_dict/serializers.py
@@ -1,6 +1,8 @@
 import json
 import pickle
-from typing import Protocol
+from typing import Final, Protocol
+
+NULL_BYTE: Final = b"\x00"
 
 
 class SharedMemoryDictSerializer(Protocol):
@@ -13,9 +15,10 @@ class SharedMemoryDictSerializer(Protocol):
 
 class JSONSerializer:
     def dumps(self, obj: dict) -> bytes:
-        return json.dumps(obj).encode()
+        return json.dumps(obj).encode() + NULL_BYTE
 
     def loads(self, data: bytes) -> dict:
+        data = bytes(data).split(NULL_BYTE, 1)[0]
         return json.loads(data)
 
 

tests/test_serializers.py
@@ -37,7 +37,7 @@ class TestJSONSerializer:
 
     @pytest.fixture
     def bytes_content(self):
-        return b'{"key": "value"}'
+        return b'{"key": "value"}\x00'
 
     @pytest.fixture
     def dict_content(self):