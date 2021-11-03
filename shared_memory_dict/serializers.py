24ec9ab84aaabbeffd22f237e2dd89c0d63c0b25
Florian Best <best@univention.de>
2021-11-03 13:55:03 +0100

    feat[serializers]: specify empty __slots__ for serializer classes
    a little bit gain of memory usage for multiple instances

--- shared_memory_dict/serializers.py
+++ shared_memory_dict/serializers.py
@@ -25,6 +25,8 @@ class SharedMemoryDictSerializer(Protocol):
 
 class JSONSerializer:
 
+    __slots__ = ()
+
     encoder = json.JSONEncoder
     decoder = json.JSONDecoder
 
@@ -43,6 +45,9 @@ class JSONSerializer:
 
 
 class PickleSerializer:
+
+    __slots__ = ()
+
     def dumps(self, obj: dict) -> bytes:
         try:
             return pickle.dumps(obj, pickle.HIGHEST_PROTOCOL)
